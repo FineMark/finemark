436c1fa642f7fe8f15c3d9891d42006d001a7e73
levish0 <shiueo.csh@gmail.com>
2026-05-17 12:06:51 +0900

    fmt

--- crates/finemark-parser/src/parser/token/mod.rs
+++ crates/finemark-parser/src/parser/token/mod.rs
@@ -1,5 +1,5 @@
-pub mod token_at;
 pub mod token_asterisk;
+pub mod token_at;
 pub mod token_backslash;
 pub mod token_caret;
 pub mod token_comma;
@@ -7,8 +7,8 @@ pub mod token_newline;
 pub mod token_tilde;
 pub mod token_undescore;
 
-pub use token_at::*;
 pub use token_asterisk::*;
+pub use token_at::*;
 pub use token_backslash::*;
 pub use token_caret::*;
 pub use token_comma::*;
