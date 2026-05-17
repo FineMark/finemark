f6aa415de9db70deb0bd35e16fffd356758abdb0
levish0 <shiueo.csh@gmail.com>
2026-05-17 10:47:31 +0900

    fix

--- crates/finemark-parser/src/parser/element.rs
+++ crates/finemark-parser/src/parser/element.rs
@@ -1,5 +1,5 @@
 use crate::parser::ParserInput;
-use crate::parser::comment::{inline_comment_parser, multiline_comment_parser};
+use crate::parser::comment::{ multiline_comment_parser};
 use crate::parser::escape::escape_parser;
 use crate::parser::text::text_parser;
 use crate::parser::token::{token_backslash_parser, token_newline_parser, token_slash};
@@ -12,7 +12,7 @@ use winnow::{Result, dispatch};
 pub(crate) fn element_parser(parser_input: &mut ParserInput) -> Result<Element> {
     dispatch! {peek(any);
         '\\' => alt((escape_parser, token_backslash_parser)),
-        '/' => alt((multiline_comment_parser, inline_comment_parser, token_slash)),
+        '/' => alt((multiline_comment_parser, token_slash)),
         '\n' => token_newline_parser,
          _ => text_parser,
     }
